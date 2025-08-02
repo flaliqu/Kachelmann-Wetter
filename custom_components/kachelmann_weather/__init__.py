ba59111b81ec2e1c4dc25f4db6ef65bf824da71f
Marcus <74002151+flaliqu@users.noreply.github.com>
2025-08-02 11:15:29 +0200

    Add files via upload

--- custom_components/kachelmann_weather/__init__.py
+++ custom_components/kachelmann_weather/__init__.py
@@ -7,9 +7,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "weather")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "weather")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
